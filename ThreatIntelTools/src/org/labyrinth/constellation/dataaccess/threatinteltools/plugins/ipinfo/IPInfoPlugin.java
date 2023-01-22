b4bf772cb575ecbab51e8db8da09fbec9b70d7a5
thom-labyrinth <107962223+thom-labyrinth@users.noreply.github.com>
2023-01-22 14:24:51 +1100

    Changing branding

--- ThreatIntelTools/src/org/labyrinth/constellation/dataaccess/threatinteltools/plugins/ipinfo/IPInfoPlugin.java
+++ ThreatIntelTools/src/org/labyrinth/constellation/dataaccess/threatinteltools/plugins/ipinfo/IPInfoPlugin.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.labyrinth.constellation.dataccess.threatinteltools.plugins.ipinfo;
+package org.labyrinth.constellation.dataaccess.threatinteltools.plugins.ipinfo;
 
 import au.gov.asd.tac.constellation.graph.processing.GraphRecordStore;
 import au.gov.asd.tac.constellation.graph.processing.GraphRecordStoreUtilities;
